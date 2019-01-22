510f91bfad0c5d399aaac55dcb82e2fbc91febdb
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-01-22 13:40:02 -0800

    using private key for stripe

--- routes/pets.js
+++ routes/pets.js
@@ -111,7 +111,7 @@ module.exports = (app) => {
     console.log(req.body);
     // Set your secret key: remember to change this to your live secret key in production
     // See your keys here: https://dashboard.stripe.com/account/apikeys
-    var stripe = require("stripe")("sk_test_Loz6xPRc7Tl8c6OCkyZMAEkE");
+    var stripe = require("stripe")(process.env.PRIVATE_STRIPE_API_KEY);
 
     // Token is created using Checkout or Elements!
     // Get the payment token ID submitted by the form:
